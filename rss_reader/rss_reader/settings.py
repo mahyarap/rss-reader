0569c103021c8d963ed03fcfb06d1fe8a13d5d35
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2019-06-21 10:41:01 +0430

    Add authnz and DRF to INSTALLED_APPS

--- rss_reader/rss_reader/settings.py
+++ rss_reader/rss_reader/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'authnz',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rss_reader.wsgi.application'
 
+AUTH_USER_MODEL = 'authnz.User'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
